395a6fe63a61377bfeea366e388aac7c3b55c5de
Faysal Ahmed <info.faysal.32@gmail.com>
2025-07-30 02:31:02 +0600

    learn dynamic rendering

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,4 +1,9 @@
-export default function About() {
+import { cookies } from "next/headers";
+
+export default async function About() {
+  const cookieStore = await cookies();
+  const theme = cookieStore.get("theme");
+  console.log(theme);
   console.log("about page in server");
   return <div>This is about page {new Date().toLocaleTimeString()}</div>;
 }
