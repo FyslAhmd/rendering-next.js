159835a470296ab95c9e5167cab58341dc53620f
Faysal Ahmed <info.faysal.32@gmail.com>
2025-07-30 19:30:33 +0600

    learn client-only code

--- src/app/server-route/page.jsx
+++ src/app/server-route/page.jsx
@@ -1,8 +1,15 @@
 import ImageSlider from "@/component/ImageSlider";
+import { clientSideFunction } from "@/utils/client-utils";
 import { serverSideFunction } from "@/utils/server-utils";
 
 export default function Server() {
   const result = serverSideFunction();
+  const res = clientSideFunction();
   console.log(result);
-  return <ImageSlider />;
+  return (
+    <>
+      {/* <h1>{res}</h1> */}
+      <ImageSlider />
+    </>
+  );
 }
